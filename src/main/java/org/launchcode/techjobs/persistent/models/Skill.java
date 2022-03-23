ef7322a6b20fd828acf1b2957ec4839b5c41304d
PhillyZ <93271937+STLPhilly@users.noreply.github.com>
2022-03-23 18:18:18 -0500

    Update Skill.java
    changing list to public instead of private

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -13,7 +13,7 @@ public class Skill extends AbstractEntity {
     public String description;
 
     @ManyToMany(mappedBy = "skills")
-    private final List<Job> jobs = new ArrayList<>();
+    public List<Job> jobs = new ArrayList<>();
 
 
     public Skill () {
